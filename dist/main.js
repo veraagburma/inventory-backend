e5250c7e3314674174288a123a0edba8e9147748
veraagburma <veraag.burma@gmail.com>
2025-12-06 18:08:01 +0800

    Included body-parser to prevent "Error: request entity too large"

--- dist/main.js
+++ dist/main.js
@@ -3,10 +3,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const dotenv = require("dotenv");
+const bodyParser = require("body-parser");
 dotenv.config();
 async function bootstrap() {
     const app = await core_1.NestFactory.create(app_module_1.AppModule);
     app.enableCors();
+    app.use(bodyParser.json({ limit: '50mb' }));
     await app.listen(3000);
     console.log(`Application is running on: ${await app.getUrl()}`);
 }
--- src/main.ts
+++ src/main.ts
@@ -2,12 +2,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
+import * as bodyParser from 'body-parser';
 
 dotenv.config();
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors(); // ← important to allow frontend to talk to backend
+  app.use(bodyParser.json({limit: '50mb'}));
   await app.listen(3000);
 
   // Below log shows localhost:3000 in console 
