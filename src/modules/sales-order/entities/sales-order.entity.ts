ebc8bcd816cb7a4efc3581077579775ede3d1efb
veraagburma <veraag.burma@gmail.com>
2025-12-06 15:39:49 +0800

    SO - Updating fields of SO and SOItem in Progress

--- src/modules/sales-order/entities/sales-order.entity.ts
+++ src/modules/sales-order/entities/sales-order.entity.ts
@@ -15,7 +15,7 @@ export class SalesOrder {
   items: SalesOrderItem[];
 
   @Column()
-  platformid: number; 
+  platform: string; 
 
   @Column({ type: 'timestamp' })
   orderdate: Date;
@@ -23,10 +23,44 @@ export class SalesOrder {
   @Column({ type: 'varchar', length: 20, default: 'Paid' })
   orderstatus: string;
 
+  @Column()
+  isfbs: boolean;
+  
+  @Column()
+  username: string;
+
+
+  @Column({ type: 'timestamp' })
+  shippeddatetime: Date;
+
+  @Column({ type: 'timestamp' })
+  deliveeddatetime: Date;
+
+  @Column({ type: 'timestamp' })
+  completeddatetime: Date;
+
+  @Column({ type: 'timestamp' })
+  cancelleddatetime: Date;
+
+
+  @Column()
+  shippingoption: string;
+
+  @Column()
+  shippingmethod: string;
+
   @Column({ type: 'numeric', precision: 12, scale: 2 })
   shippingfee: number;
 
+  @Column({ type: 'numeric', precision: 12, scale: 2 })
+  shippingfeesaverprogramme: number;
 
+
+  @Column({ type: 'numeric', precision: 12, scale: 2 })
+  orderqty: number;
+
+  @Column({ type: 'numeric', precision: 12, scale: 2 })
+  totalprice: number;
 
 
   @Column({ type: 'numeric', precision: 12, scale: 2 })
@@ -37,30 +71,23 @@ export class SalesOrder {
 
   @Column({ type: 'numeric', precision: 12, scale: 2 })
   transactionfee : number;
-  
-  @Column({ type: 'numeric', precision: 12, scale: 2 })
-  shippingfeesaverprogramme: number;
 
-
-  @Column({ type: 'numeric', precision: 12, scale: 2 })
-  totalprice: number;
 
   @Column({ type: 'numeric', precision: 12, scale: 2 })
   sellerbundlediscount: number;
 
   @Column({ type: 'numeric', precision: 12, scale: 2 })
-  sellervoucher: number;
-
-
-  @Column({ type: 'numeric', precision: 12, scale: 2 })
   platformbundlediscount: number;
+
   
   @Column({ type: 'numeric', precision: 12, scale: 2 })
-  platformvouchervalue: number;
-
+  sellervoucheramount: number;
+  
+  @Column({ type: 'numeric', precision: 12, scale: 2 })
+  platformvoucheramount: number;
 
   @Column({ type: 'numeric', precision: 12, scale: 2 })
-  creditcarddiscount: number;
+  sellerabsorbedvalue: number;
 
 
   @Column()
